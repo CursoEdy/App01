bbdeace42d07d7a33af9a30c2d982aafc53e8dcd
Edy <30.edyalves@gmail.com>
2023-08-02 18:41:13 -0300

    Primeiro commit App01

---
+++ src/test/java/com/edyalves/app01/App01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edyalves.app01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
